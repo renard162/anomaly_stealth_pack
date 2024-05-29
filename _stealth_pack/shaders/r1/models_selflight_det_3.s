eb6dc2606e0d8d5d6c487820fb65ad3efc0d1555
Samuel Oliveira <renard162@gmail.com>
2024-05-29 15:06:21 -0300

    Revert "Adicionado mod swm_visible_body"
    This reverts commit 1994c74d52cc94de3bbdb57506b995cf4d59c7f7.

--- _stealth_pack/shaders/r1/models_selflight_det_3.s
+++
@@ -1,7 +0,0 @@
-function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("model_def_hq","base_lplanes")
-			: fog		(true)
-	shader:sampler	("s_base")      :texture	(t_base)
-	shader:sampler	("s_bump")      :texture	(t_base.."_bump")
-	shader:sampler	("s_bumpX")     :texture	(t_base.."_bump#")
-end
