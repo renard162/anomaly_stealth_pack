91a383a5f5649472cbed1d581684fe21e3e67940
Samuel Oliveira <renard162@gmail.com>
2025-03-29 18:21:41 -0300

    Alterado script de compilacao do launcher para o novo local do venv

--- _launcher/build_launcher.bat
+++ _launcher/build_launcher.bat
@@ -1,6 +1,6 @@
 @echo off
 del .\dist\launcher.exe
 del .\dist\WelcomeLauncher.exe
-.\venv\Scripts\pyinstaller.exe --onefile --icon=icon.ico launcher.py
+..\..\venv\Scripts\pyinstaller.exe --onefile --icon=icon.ico launcher.py
 ren .\dist\launcher.exe WelcomeLauncher.exe
 pause
