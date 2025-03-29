69aafa8e2625b79ecbe971b0e3e205b51e3905cb
Samuel Oliveira <renard162@gmail.com>
2025-03-28 21:59:03 -0300

    Adicionado script para fazer o build do Launcher

---
+++ _launcher/build_launcher.bat
@@ -0,0 +1,6 @@
+@echo off
+del .\dist\launcher.exe
+del .\dist\WelcomeLauncher.exe
+.\venv\Scripts\pyinstaller.exe --onefile --icon=icon.ico launcher.py
+ren .\dist\launcher.exe WelcomeLauncher.exe
+pause
