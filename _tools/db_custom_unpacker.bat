74e53f546ee750fbdd14b34a388005050a1f3587
Samuel Oliveira <renard162@gmail.com>
2025-04-19 17:20:02 -0300

    Mudanca no script de extracao de DB custom

--- _tools/db_custom_unpacker.bat
+++ _tools/db_custom_unpacker.bat
@@ -1,8 +1,15 @@
 @echo off
-mkdir .\_unpacked
-for %%f in (..\..\..\db\mods\*.db*) do (
+mkdir .\_unpacked_full
+for %%f in (..\..\..\db\*.db*) do (
     echo Unpacking %%f
-    converter.exe -unpack -xdb %%f -dir .\_unpacked
+    converter.exe -unpack -xdb %%f -dir .\_unpacked_full
 )
-
+for %%f in (..\..\..\db\textures\*.db*) do (
+    echo Unpacking %%f
+    converter.exe -unpack -xdb %%f -dir .\_unpacked_full
+)
+for %%f in (..\..\..\db\patches\*.db*) do (
+    echo Unpacking %%f
+    converter.exe -unpack -xdb %%f -dir .\_unpacked_full
+)
 pause
